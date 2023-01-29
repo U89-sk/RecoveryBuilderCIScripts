68c4ad44171c22fa6b016166716ff5274643cf57
U89-sk <107832346+U89-sk@users.noreply.github.com>
2023-01-29 16:18:03 +0530

    Create rombuild.sh

---
+++ rombuild.sh
@@ -0,0 +1,13 @@
+echo "============================"
+echo "Starting the Build..."
+echo "============================"
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+ccache -M 50G
+export LC_ALL=C   
+. build/envsetup.sh
+export CONFIG_STATE_NOTIFIER=y
+export SELINUX_IGNORE_NEVERALLOWS=true
+export ALLOW_MISSING_DEPENDENCIES=true
+lunch $LUNCH_COMBO
+make -j31 $TARGET
